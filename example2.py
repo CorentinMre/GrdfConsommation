4b8a2a1ff064316d057d0f646b724209e46d5ca8
CorentinMre <corentin.marie@laposte.net>
2022-07-31 21:25:27 +0200

    example

--- example2.py
+++ example2.py
@@ -5,9 +5,9 @@ import datetime
 
 profil = GrdfConsommation.GRDFConsommation(
                                             email="<your email>", 
-                                            password="<your password>", 
-                                            pce_id="<your pce id>", 
-                                            nbDays=10 # Nombre de jours à récupérer depuis ce jour
+                                            password="<your password>",
+                                            nbDays=10, # Nombre de jours à récupérer depuis ce jour
+                                            pce_id="<your pce id>" # Pour plus de rapidité vous pouvez entrez votre identifiant pce (mais il n'est pas obligatoire)
                                         )
 
 #données des n derniers jours
